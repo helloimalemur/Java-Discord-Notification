a303857d838d9e7e113611c92d08c2e54b052b56
foxx <jkoontsiii@gmail.com>
2022-09-01 21:31:57 -0400

    .

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        String testString = "test string";
+        DiscordNotif discordNotif = new DiscordNotif();
+        discordNotif.sendNotif(testString);
+    }
+}
